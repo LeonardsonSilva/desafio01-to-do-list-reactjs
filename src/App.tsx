1b7f559725c0d2da02dd5899ba27830582c77918
Leonardson Silva <leonardsonsilva.dit@gmail.com>
2022-09-04 20:03:36 -0400

    feat: input new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './global.css'
 import {Header} from './components/Header'
+import styles from './App.module.css'
+import {PlusCircle} from 'phosphor-react'
 export function App() {
 
   return (
     <>
       <Header/>
+
+      <div className={styles.wrapper}>
+        <form action="#" className={styles.formNewTask}>
+          <input type="text" placeholder='Adicione uma nova tarefa'/>
+          <button type='submit' >
+            Criar <PlusCircle size={16}/>
+          </button>
+        </form>
+      </div>
+
     </>
   )
 }
